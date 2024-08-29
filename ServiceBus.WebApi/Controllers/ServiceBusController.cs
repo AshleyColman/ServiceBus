f2c0faaf7689fd5da30f55227bcb0ebb7a2aff8c
azure <azure@DESKTOP-6VKB937>
2024-08-29 07:28:18 +0100

    Add project files.

---
+++ ServiceBus.WebApi/Controllers/ServiceBusController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ServiceBus.WebApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ServiceBusController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetMessage()
+        {
+            return Ok();
+        }
+
+        [HttpPost]
+        public IActionResult PostMessageToQueue()
+        {
+
+        }
+    }
+}
